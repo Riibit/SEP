cce8809d3061aababce337b2f09d6ec4675f1259
Julian Rudolf <julian.rudolf@student.tugraz.at>
2017-03-23 15:20:56 +0100

    Add ECondition

---
+++ EnvironmentalCondition.h
@@ -0,0 +1,10 @@
+//------------------------------------------------------------------------------
+// EnvironmentalCondition.h
+//
+// Group: Group 15666, study assistant Hasan Durmaz
+//
+// Authors: Milan Drinic 143****
+// Christopher Hinterer 1432027
+// Julian Rudolf 1331657
+//------------------------------------------------------------------------------
+//
