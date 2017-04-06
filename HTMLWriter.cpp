14649b3f27e6ac07b4705ada20bdfcc1d7294192
Christopher Marc Hinterer <hinterer@student.tugraz.at>
2017-04-06 16:48:56 +0200

    Made minor improvements

--- HTMLWriter.cpp
+++ HTMLWriter.cpp
@@ -34,17 +34,13 @@ void HTMLWriter::writeFile(string body)
   writer.open (filename_.c_str());
   if(writer.is_open())
   {
-    writer << "<!DOCTYPE html>" << endl;
-    writer << "<html lang=\"en\">" << endl;
-    writer << "<head>" << endl;
-    writer << "<meta charset=\"utf-8\">" << endl;
-    writer << "<meta http-equiv=\"refresh\" content=\"3\">" << endl;
-    writer << "<title>SEP 2017</title>" << endl;
-    writer << "<style>body{margin: 0px; padding: 0px;}</style>" << endl;
-    writer << "</head>" << endl;
-    writer << "<body>" << "<h1><center>LIMOSTANDL</center></h1>" << body;
-    writer << "</body>" << endl;
-    writer << "</html>";
+    writer << "<!DOCTYPE html>" << endl << "<html lang=\"en\">" << endl
+      << "<head>" << endl << "<meta charset=\"utf-8\">" << endl
+      << "<meta http-equiv=\"refresh\" content=\"3\">" << endl
+      << "<title>SEP 2017</title>" << endl 
+      << "<style>body{margin: 0px; padding: 0px;}</style>" << endl << "</head>" 
+      << endl << "<body>" << "<h1><center>LIMOSTANDL</center></h1>" << body
+      << "</body>" << endl << "</html>";
     writer.close();
   }
 }
