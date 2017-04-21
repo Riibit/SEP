d544e9c9e8124d667665a552a91506d2a6fa0af7
Christopher Marc Hinterer <hinterer@student.tugraz.at>
2017-04-21 17:57:49 +0200

    Added Files for d3

---
+++ Command.h
@@ -0,0 +1,61 @@
+//------------------------------------------------------------------------------
+// Command.h
+//
+// Group: Group 15666, study assistant Hasan Durmaz
+//
+// Authors: Milan Drinic 1431883
+// Christopher Hinterer 1432027
+// Julian Rudolf 1331657
+//------------------------------------------------------------------------------
+//
+
+#ifndef COMMAND_H_INCLUDED
+#define COMMAND_H_INCLUDED
+
+#include <string>
+#include <vector>
+
+class GameHandler;
+
+class Command
+{
+  private:
+    //--------------------------------------------------------------------------
+    // Private copy constructor
+
+    Command(const Command& original);
+
+    //--------------------------------------------------------------------------
+    // Private assignment operator
+
+    Command& operator=(const Command& original);
+    
+    //--------------------------------------------------------------------------
+    /// Name of this command
+    std::string command_name_;
+
+  public:
+    //--------------------------------------------------------------------------
+    // Constructor
+
+    Command(std::string name);
+
+    //--------------------------------------------------------------------------
+    // Destructor
+
+    virtual ~Command();
+
+    //--------------------------------------------------------------------------
+    // Executes the command.
+    // @param game The game where action should be performed on
+    // @param params Possible parameters neede for the execution
+    // @return Integer representing the success of the action
+
+    virtual int execute(GameHandler& game, std::vector<std::string>& params) = 0;
+    
+    //--------------------------------------------------------------------------
+    // Getter Methods
+    const std::string& getName() const;
+};
+
+#endif //COMMAND_H_INCLUDED
