c670b2ad7e93a090a854797f310c6b3e97693ffe
Christopher Marc Hinterer <hinterer@student.tugraz.at>
2017-04-24 16:00:47 +0200

    Corrected some Grammar

--- Command.h
+++ Command.h
@@ -48,7 +48,7 @@ class Command
     //--------------------------------------------------------------------------
     // Executes the command.
     // @param game The game where action should be performed on
-    // @param params Possible parameters neede for the execution
+    // @param params Possible parameters needed for the execution
     // @return Integer representing the success of the action
 
     virtual int execute(GameHandler& game, std::vector<std::string>& params) = 0;
