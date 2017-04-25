3698028f52442e75cbd9bc2401a80435e95e1101
Milan Drinic <milan.drinic@student.tugraz.at>
2017-04-25 21:57:16 +0200

    gamehandler

---
+++ GameHandler.cpp
@@ -0,0 +1,14 @@
+//------------------------------------------------------------------------------
+//GameHandler.cpp
+//
+// Group: Group 15666, study assistant Hasan Durmaz
+//
+// Authors: Milan Drinic 1431883
+// Christopher Hinterer 1432027
+// Julian Rudolf 1331657
+//------------------------------------------------------------------------------
+//
+
+#include "GameHandler.h"
+
+
---
+++ GameHandler.h
@@ -0,0 +1,24 @@
+//------------------------------------------------------------------------------
+// GameHandler.h
+//
+// Group: Group 15666, study assistant Hasan Durmaz
+//
+// Authors: Milan Drinic 1431883
+// Christopher Hinterer 1432027
+// Julian Rudolf 1331657
+//------------------------------------------------------------------------------
+//
+
+#ifndef GAMEHANDLER_H
+#define GAMEHANDLER_H
+
+class GameHandler 
+{
+  public:
+
+
+  private:
+
+};
+
+#endif //GAMEHANDLER_H
