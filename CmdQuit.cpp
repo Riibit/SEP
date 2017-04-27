d6bf7447039a85a73a59bfcb029e2f0ac58b0eb4
Julian Rudolf <julian.rudolf@student.tugraz.at>
2017-04-27 13:10:10 +0200

    Added quit usage errors

--- CmdQuit.cpp
+++ CmdQuit.cpp
@@ -15,9 +15,10 @@
 
 const std::string CmdQuit::QUIT_MSG = "Going out of business!";
 const std::string CmdQuit::CMD_NAME = "quit";
+const std::string CmdQuit::ERR_QUIT = "[ERR] Usage: quit";
 static const unsigned int PARA_COUNT = 0;
 
-CmdQuit::CmdQuit() : Command::Command(CMD_NAME, PARA_COUNT, "")
+CmdQuit::CmdQuit() : Command::Command(CMD_NAME, PARA_COUNT, ERR_QUIT)
 {
 }
 
