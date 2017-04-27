a8ba1f806d2699170dbcbdc0cada48bf9249a2ac
Milan Drinic <milan.drinic@student.tugraz.at>
2017-04-27 13:11:34 +0200

    Interface coding standard

--- Interface.h
+++ Interface.h
@@ -17,15 +17,40 @@
 
 class GameHandler; 
 
+//----------------------------------------------------------------------------
+// Interface Class
+// class representing the user interface/view
+//
 class Interface 
 {
   public:
+
+    //------------------------------------------------------------------------
+    // Constructor
+    // @param game Pointer to the calling object
+    //
     Interface(GameHandler* game);
+
+    //------------------------------------------------------------------------
+    // Initializes the interface
+    //  
     const int runPrompt();
+
+    //------------------------------------------------------------------------
+    // Acquires command name
+    // @param cin_value Input item 
+    // @param to_lower Lowercase flag
+    //
     const std::string getArgument(char* cin_value, bool to_lower);
+    
+    //------------------------------------------------------------------------
+    // Pushes command parameters to a vector
+    // @param cin_value Input item
+    //
     void makeVector(char* cin_value);
 
     static const std::string PROMPT_STRING;
+    
   private:
     GameHandler* game_;
 };
