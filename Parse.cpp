d8c2a2566c41d1238b678da172de6e6d56ade73a
Julian Rudolf <julian.rudolf@student.tugraz.at>
2017-05-04 20:04:08 +0200

    Added files for parsing values

---
+++ Parse.cpp
@@ -0,0 +1,25 @@
+//------------------------------------------------------------------------------
+// Parse.cpp
+//
+// Group: Group 15666, study assistant Hasan Durmaz
+//
+// Authors: Milan Drinic 1431883
+// Christopher Hinterer 1432027
+// Julian Rudolf 1331657
+//------------------------------------------------------------------------------
+//
+
+int parseInteger(std::string string)
+{
+  int parsed_integer;
+  std::locale loc;
+  for (unsigned int position = 0; position < string.size(); ++position)
+  {
+    if (!std::isdigit(string[position], loc))
+    {
+      return -1;
+    }
+  }
+  parsed_integer = std::stoi(string);
+  return parsed_integer;
+}
