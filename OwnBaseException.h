edc5f16f91d518a44c330c0bd589e7c029936ea0
Christopher Marc Hinterer <hinterer@student.tugraz.at>
2017-05-05 10:33:11 +0200

    Added OwnBaseException header

---
+++ OwnBaseException.h
@@ -0,0 +1,25 @@
+//------------------------------------------------------------------------------
+// OwnBaseException.h
+//
+// Group: Group 15666, study assistant Hasan Durmaz
+//
+// Authors: Milan Drinic 1431883
+// Christopher Hinterer 1432027
+// Julian Rudolf 1331657
+//------------------------------------------------------------------------------
+//
+ 
+#ifndef OWNBASEEXCEPTION_H_INCLUDED
+#define OWNBASEEXCEPTION_H_INCLUDED
+ 
+#include <exception>
+
+//------------------------------------------------------------------------------
+// Base for all exceptions
+//
+class OwnBaseException : std::exception
+{
+
+};
+ 
+#endif // OWNBASEEXCEPTION_H_INCLUDED
