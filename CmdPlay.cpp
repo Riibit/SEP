db3e97e4048422fdc9c6564d668a726b85fcc95b
Milan Drinic <milan.drinic@student.tugraz.at>
2017-05-05 12:16:08 +0200

    idk merge conflicts

---
+++ CmdPlay.cpp
@@ -0,0 +1,24 @@
+//------------------------------------------------------------------------------
+// CmdPlay.cpp
+//
+// Group: Group 15666, study assistant Hasan Durmaz
+//
+// Authors: Milan Drinic 1431883
+// Christopher Hinterer 1432027
+// Julian Rudolf 1331657
+//------------------------------------------------------------------------------
+//
+
+#include "CmdPlay.h"
+
+const std::string CmdPlay::CMD_NAME = "play";
+
+CmdPlay::CmdPlay() : Command::Command(CMD_NAME)
+{
+}
+
+int CmdPlay::execute(GameHandler& game, std::vector<std::string>& params)
+{
+
+  return 0;
+}
