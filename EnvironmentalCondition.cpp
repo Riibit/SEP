3101ebd3ef51000b6bee8fa499f90fd3d1ec6932
Julian Rudolf <julian.rudolf@student.tugraz.at>
2017-05-08 20:03:34 +0200

    CS and deconstructor in EnvCond.*

--- EnvironmentalCondition.cpp
+++ EnvironmentalCondition.cpp
@@ -12,22 +12,15 @@
 #include "EnvironmentalCondition.h"
 #include "EnvironmentalEngine.h"
 
-//------------------------------------------------------------------------------
-// Default constructor for the class
-//
 EnvironmentalCondition::EnvironmentalCondition()
 {
-
 }
 
-//------------------------------------------------------------------------------
-// Constructor for the class
-//
-// @param sky_cover The sky cover rank
-// @param precipitation The precipitation rank
-// @param temperature The temperature
-// @param wind The wind rank
-//
+EnvironmentalCondition::~EnvironmentalCondition()
+{
+}
+
+
 EnvironmentalCondition::EnvironmentalCondition(Cover sky_cover, 
   Rank precipitation, float temperature, Rank wind)
 {
@@ -37,12 +30,6 @@ EnvironmentalCondition::EnvironmentalCondition(Cover sky_cover,
   setWind(wind);
 }
 
-//------------------------------------------------------------------------------
-// Method to find out if it is sunny
-//
-// @return bool True if it is sunny and the temperature is in the top one 
-//         third
-//
 bool EnvironmentalCondition::isItHot()
 {
   if (sky_cover_ == SUNNY && temperature_ > (HOT_THRESHOLD))
@@ -55,12 +42,6 @@ bool EnvironmentalCondition::isItHot()
   }
 }
 
-//------------------------------------------------------------------------------
-// Method to find out if it is rainy
-//
-// @return bool True if the precipation is medium or high and the sky cover
-//         is overcast or very overcast
-//
 bool EnvironmentalCondition::isItRainy()
 {
   if ((precipitation_ == MEDIUM || precipitation_ == HIGH)
@@ -74,12 +55,6 @@ bool EnvironmentalCondition::isItRainy()
   } 
 }
 
-//------------------------------------------------------------------------------
-// Method to find out if it is stormy
-//
-// @return bool True if the wind is medium or high and the sky cover is 
-//         overcast or very overcast
-//
 bool EnvironmentalCondition::isItStormy()
 {
   if ((wind_ == MEDIUM || wind_ == HIGH) 
@@ -93,51 +68,26 @@ bool EnvironmentalCondition::isItStormy()
   }
 }
 
-//------------------------------------------------------------------------------
-// Method to set sky cover rank
-//
-// @param cover The sky cover rank that is to be set
-//
 void EnvironmentalCondition::setSkyCover(Cover cover)
 {
   sky_cover_ = cover;
 }
 
-//------------------------------------------------------------------------------
-// Method to get the sky cover rank
-//
-// @return EnvironmentalCondition::Cover The sky cover rank
-//
 EnvironmentalCondition::Cover EnvironmentalCondition::getSkyCover(void)
 {
   return sky_cover_;
 }
 
-//------------------------------------------------------------------------------
-// Method to set the precipitation rank
-//
-// @param rank The precipitation rank that is to be set
-//
 void EnvironmentalCondition::setPrecipitation(Rank rank)
 {
   precipitation_ = rank;
 }
 
-//------------------------------------------------------------------------------
-// Method to get the precipitation rank
-//
-// @return EnvironmentalCondition::Rank The precipitation rank
-//
 EnvironmentalCondition::Rank EnvironmentalCondition::getPrecipitation(void)
 {
   return precipitation_;
 }
 
-//------------------------------------------------------------------------------
-// Method to set the temperature
-//
-// @param temperature the value of the temperature that is to be set
-//
 void EnvironmentalCondition::setTemperature(float temperature)
 {
   if (temperature_ >= MAX_TEMP || temperature_ <= MIN_TEMP)
@@ -146,31 +96,16 @@ void EnvironmentalCondition::setTemperature(float temperature)
   }
 }
 
-//------------------------------------------------------------------------------
-// Method to get the temperature
-//
-// @return float The temperature
-//
 float EnvironmentalCondition::getTemperature(void)
 {
   return temperature_;
 }
 
-//------------------------------------------------------------------------------
-// Method to set the wind rank
-//
-// @param rank The wind rank that is to be set
-//
 void EnvironmentalCondition::setWind(Rank rank)
 {
   wind_ = rank;
 }
 
-//------------------------------------------------------------------------------
-// Method to get the wind rank
-//
-// @return EnvironmentalCondition::Rank The wind rank
-//
 EnvironmentalCondition::Rank EnvironmentalCondition::getWind(void)
 {
   return wind_;
--- EnvironmentalCondition.h
+++ EnvironmentalCondition.h
@@ -22,27 +22,110 @@ public:
   static const int MAX_TEMP = 35;
   static const int HOT_THRESHOLD = (((MAX_TEMP - MIN_TEMP) / 3) + MIN_TEMP);
 
+  //--------------------------------------------------------------------------
+   // Constructor
   EnvironmentalCondition();
+
+  //--------------------------------------------------------------------------
+  // Destructor
+  ~EnvironmentalCondition();
 
   enum Cover {SUNNY, CLOUDY, OVERCAST, VERY_OVERCAST};
   enum Rank {NONE, LOW, MEDIUM, HIGH};
 
+
+  //------------------------------------------------------------------------------
+  // Constructor for the class
+  //
+  // @param sky_cover The sky cover rank
+  // @param precipitation The precipitation rank
+  // @param temperature The temperature
+  // @param wind The wind rank
+  //
   EnvironmentalCondition(Cover sky_cover_, Rank precipitation_,
     float temperature_, Rank wind_);
 
+  //------------------------------------------------------------------------------
+  // Method to set sky cover rank
+  //
+  // @param cover The sky cover rank that is to be set
+  //
   void setSkyCover(Cover cover);
+
+  //------------------------------------------------------------------------------
+  // Method to set the precipitation rank
+  //
+  // @param rank The precipitation rank that is to be set
+  //
   void setPrecipitation(Rank rank);
+
+  //------------------------------------------------------------------------------
+  // Method to set the temperature
+  //
+  // @param temperature the value of the temperature that is to be set
+  //
   void setTemperature(float temperature);
+
+  //------------------------------------------------------------------------------
+  // Method to set the wind rank
+  //
+  // @param rank The wind rank that is to be set
+  //
   void setWind(Rank rank);
 
+
+  //------------------------------------------------------------------------------
+  // Method to get the sky cover rank
+  //
+  // @return EnvironmentalCondition::Cover The sky cover rank
+  //
   Cover getSkyCover(void);
+
+  //------------------------------------------------------------------------------
+  // Method to get the precipitation rank
+  //
+  // @return EnvironmentalCondition::Rank The precipitation rank
+  //
   Rank getPrecipitation(void);
+
+  //------------------------------------------------------------------------------
+  // Method to get the temperature
+  //
+  // @return float The temperature
+  //
   float getTemperature(void);
+
+  //------------------------------------------------------------------------------
+  // Method to get the wind rank
+  //
+  // @return EnvironmentalCondition::Rank The wind rank
+  //
   Rank getWind(void);
 
+  //------------------------------------------------------------------------------
+  // Method to find out if it is sunny
+  //
+  // @return bool True if it is sunny and the temperature is in the top one 
+  //         third
+  //
   bool isItHot();
+
+  //------------------------------------------------------------------------------
+  // Method to find out if it is rainy
+  //
+  // @return bool True if the precipation is medium or high and the sky cover
+  //         is overcast or very overcast
+  //
   bool isItRainy();
+
+  //------------------------------------------------------------------------------
+  // Method to find out if it is stormy
+  //
+  // @return bool True if the wind is medium or high and the sky cover is 
+  //         overcast or very overcast
+  //
   bool isItStormy();
+
 private:
   Cover sky_cover_;
   Rank precipitation_;
