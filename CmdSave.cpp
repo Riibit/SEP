4484d552f24d694a9d5559a39e9d0729194c44a6
Milan Drinic <milan.drinic@student.tugraz.at>
2017-05-11 22:10:39 +0200

    save command added indentation kind of v1.1

--- CmdSave.cpp
+++ CmdSave.cpp
@@ -31,28 +31,28 @@ int CmdSave::execute(GameHandler& game, std::vector<std::string>& params)
 
   savefile << "<savefile>" << std::endl;
 
-  savefile << "<weather>" << std::endl;
+  savefile << "  <weather>" << std::endl;
 
-  savefile << "<wind>" << " "/*game.getEnvCond.wind*/ << "</wind>" << std::endl;
-  savefile << "<temperature>" <<" " /*game.getEnvCond.temp*/  << "</temperature>" << std::endl;
-  savefile << "<precipitation>" << " "/*game.getEnvCond.prec*/ << "</precipitation>" << std::endl;
-  savefile << "<cover>" << " "/*game.getEnvCond.cover*/ << "</cover>" << std::endl;
+  savefile << "    <wind>" << " "/*game.getEnvCond.wind*/ << "</wind>" << std::endl;
+  savefile << "    <temperature>" <<" " /*game.getEnvCond.temp*/  << "</temperature>" << std::endl;
+  savefile << "    <precipitation>" << " "/*game.getEnvCond.prec*/ << "</precipitation>" << std::endl;
+  savefile << "    <cover>" << " "/*game.getEnvCond.cover*/ << "</cover>" << std::endl;
 
-  savefile << "</weather>" << std::endl;
+  savefile << "  </weather>" << std::endl;
   
-  savefile << "<stats>" << std::endl;
+  savefile << "  <stats>" << std::endl;
 
-  savefile << "<cash>" << game.getResourceMoney() << "</cash>" << std::endl;
-  savefile << "<balance>" << game.getResourceBalance() << "</balance>" << std::endl;
-  savefile << "<income>" << " "/*game.getIncome()*/ << "</income>" << std::endl;
-  savefile << "<expense>" << game.getExpenses() << "</expense>" << std::endl;
-  savefile << "<lemons>" << game.getResourceLemon() << "</lemons>" << std::endl;
-  savefile << "<sugar>" << game.getResourceSugar() << "</sugar>" << std::endl;
-  savefile << "<pricelemons>" << game.getResourceLemonade() << "</pricelemons>" << std::endl;
-  savefile << "<pricesugar>" << game.getPriceSugar() << "</pricesugar>" << std::endl;
-  savefile << "<pricelemons>" << game.getPriceLemon() << "</pricelemons>" << std::endl;
-  savefile << "<pricelemonade>" << game.getPriceLemonade() << "</pricelemonade>" << std::endl;
-  savefile << "</stats>" << std::endl;
+  savefile << "    <cash>" << game.getResourceMoney() << "</cash>" << std::endl;
+  savefile << "    <balance>" << game.getResourceBalance() << "</balance>" << std::endl;
+  savefile << "    <income>" << " "/*game.getIncome()*/ << "</income>" << std::endl;
+  savefile << "    <expense>" << game.getExpenses() << "</expense>" << std::endl;
+  savefile << "    <lemons>" << game.getResourceLemon() << "</lemons>" << std::endl;
+  savefile << "    <sugar>" << game.getResourceSugar() << "</sugar>" << std::endl;
+  savefile << "    <pricelemons>" << game.getResourceLemonade() << "</pricelemons>" << std::endl;
+  savefile << "    <pricesugar>" << game.getPriceSugar() << "</pricesugar>" << std::endl;
+  savefile << "    <pricelemons>" << game.getPriceLemon() << "</pricelemons>" << std::endl;
+  savefile << "    <pricelemonade>" << game.getPriceLemonade() << "</pricelemonade>" << std::endl;
+  savefile << "  </stats>" << std::endl;
 
   savefile << "</savefile>" << std::endl;
   /*if(!savefile)
