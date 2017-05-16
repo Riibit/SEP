5d1a541b5f7aa404bacab2d695f6baee7c5f93a9
Julian Rudolf <julian.rudolf@student.tugraz.at>
2017-05-16 17:49:36 +0200

    ReturnValues.h

--- ReturnValues.h
+++ ReturnValues.h
@@ -8,7 +8,13 @@
 // Julian Rudolf 1331657
 //------------------------------------------------------------------------------
 //
+#ifndef RETURN_VALUES_H
+#define RETURN_VALUES_H
+
+#define AUFBAU
 
 static const int RETURN_SUCCESS = 0;
 static const int RETURN_OUT_OF_MEMORY = 1;
 static const int RETURN_WRONG_USAGE = 2;
+
+#endif //RETURN_VALUES_H
